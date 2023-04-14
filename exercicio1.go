9916af9c9de047bee7cee4d3dfd1301e261b884d
PedroDiOliveira <pedro.h.soterio@gmail.com>
2023-04-13 23:38:41 -0300

    fimDosSequenciais

--- exercicio1.go
+++ exercicio1.go
@@ -1,1 +1,15 @@
-package algoritmos___strings_PedroDiOliveira
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var str string
+	//perguntando ao usuario
+	fmt.Println("Qual a string?")
+	fmt.Scan(&str)
+	conversao := strings.ToUpper(str)
+	fmt.Println("A sua string convertida para maiusculo fica:", conversao)
+}
--- exercicio2.go
+++ exercicio2.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var str string
+	fmt.Println("Digite uma string:")
+	fmt.Scan(&str)
+
+	newStr := strings.Join(strings.Fields(str), " ")
+
+	fmt.Printf("A nova string é: %s\n", newStr)
+}
